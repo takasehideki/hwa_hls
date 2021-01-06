87d4dd061ec0bff8715c8c556168f18a22191a90
takasehideki <takase@i.kyoto-u.ac.jp>
2021-01-06 12:10:04 +0900

    precise bitwidth

--- lec13/fir_6_bitwidth.h
+++ lec13/fir_6_bitwidth.h
@@ -1,7 +1,7 @@
 #include "ap_int.h"
 
-typedef ap_int<16> coef_t;
-typedef ap_int<16> data_t;
-typedef ap_int<24> acc_t;
+typedef ap_int<10> coef_t;
+typedef ap_uint<16> data_t;
+typedef ap_uint<20> acc_t;
 
 void fir(data_t *y, data_t x);
